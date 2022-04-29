4ce4381eaf47a87de96b4cbaa92fe18d418ff7ce
Luan Carlo <32052204+lluancarlo@users.noreply.github.com>
2022-04-29 22:45:53 +0200

    Delete DravenCareca/DravenCareca directory

--- DravenCareca/DravenCareca/Dto/BotConfigDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace DravenCareca.Dto
-{
-    public class BotConfigDto
-    {
-        public string token { get; set; }
-        public string prefix { get; set; }
-        public string game { get; set; }
-    }
-}
